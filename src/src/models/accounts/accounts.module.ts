c95a4d2535248ae423983d3842095ee725ef436d
0n3m0r3 <louka.altdorfreynes@gmail.com>
2024-07-17 15:35:36 +0200

    fix: Add FilesAzureService provider to AccountsModule
    This commit adds the FilesAzureService provider to the AccountsModule in the accounts.module.ts file. The FilesAzureService is responsible for handling file operations related to Azure storage. This change enables the module to utilize the FilesAzureService for file-related functionality, enhancing the overall functionality of the module.

--- src/src/models/accounts/accounts.module.ts
+++ src/src/models/accounts/accounts.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
 import { PrismaService } from '../../prisma/prisma.service';
+import { FilesAzureService } from '../validate/files-azure.service';
 
 @Module({
   controllers: [AccountsController],
-  providers: [AccountsService, PrismaService],
+  providers: [AccountsService, PrismaService, FilesAzureService],
 })
 export class AccountsModule {}
